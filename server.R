7835843ca48d189273e624a67c6daf97630e826d
Benedikt Brink <bgbrink@users.noreply.github.com>
2018-07-08 15:37:51 +0200

    Update server.R

--- server.R
+++ server.R
@@ -25,7 +25,7 @@ shinyServer(function(input, output, session) {
   if (Sys.info()['sysname'] == "Windows") {
     nrOfCores <- 1
   } else {
-    nrOfCores <- detectCores()/4
+    nrOfCores <- floor(detectCores()/3)
   }
   
   # reactive table for rhandsontable
